a0b7144a8169b342d575789269a80e24048cc5df
EcsFlash <55760802+EcsFlash@users.noreply.github.com>
2024-12-31 19:45:07 +0300

    вроде написал линейный однонаправленный список

--- LList.h
+++ LList.h
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <sstream>
 using namespace std;
+
+
 template<typename T>
 class LList {
 	struct Node {
@@ -15,17 +18,84 @@ public:
 	LList() {
 		head = nullptr;
 	}
+	LList(T arr[], int size, bool reversed) {
+		if (!reversed) {
+			for (int i = 0; i < size; i++) {
+				addToHead(arr[i]);
+			}
+		}
+		else {
+			for (int i = size; i >= 0; i--) {
+				addToHead(arr[i]);
+			}
+		}
+	}
+	void extendSorted(T arr[], int size) {
+		int temp;
+		for (int i = 0; i < size; i++) {
+			for (int j = i; j < size-1; j++) {
+				if (arr[i] < arr[j]) {
+					temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+		for (int i = 0; i < size; i++) {
+			addToHead(arr[i]);
+		}
+	}
+	~LList() {
+		clear();
+	}
 	void addToHead(T element) {
 		//Node* temp = head;
 		Node* newHead = new Node(element);
 		newHead->next = head;
 		head = newHead;
 	}
-	void print() {
+	string toString() const {
+		stringstream result;
 		Node* temp = head;
-		while(temp != nullptr){
-			cout << temp->data << endl;
+		while (temp != nullptr) {
+			result << temp->data << " ";
 			temp = temp->next;
 		}
+		return result.str();
+	}
+	void clear() {
+		while (!isEmpty())
+		{
+			deleteFirst();
+		}
+	}
+	bool isEmpty() {
+		return head == nullptr;
+	}
+	void deleteFirst() {
+		if (!isEmpty()) {
+			Node* temp = head->next;
+			delete head;
+			head = temp;
+		}
+	}
+	void deleteAfter(Node* whereTo) {
+		if (!isEmpty() && whereTo) {
+			Node* temp = whereTo->next;
+			if (temp) {
+				whereTo->next = whereTo->next->next;
+				delete temp;
+			}
+		}
+	}
+	void addAfter(Node* whereTo, T element) {
+		if (!isEmpty() && whereTo) {
+			Node* newElement = new Node(element);
+			newElement->next = whereTo->next;
+			whereTo->next = newElement;
+		}
+	}
+	friend ostream& operator<<(ostream& os, const LList& list) {
+		return os << list.toString();
 	}
 };
--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,15 @@
 #include "LList.h"
+struct I {
+	int i;
+};
 
-
+I sp[6];
 int main() {
 	LList<int> list;
 	list.addToHead(2);
 	list.addToHead(2);
 	list.addToHead(2);
-	list.print();
+	list.extendSorted(new int[6] { 2, 6, 3, 8, 9, 1 }, 6);
+	cout << list << endl;
+
 }
