7a973f327c7405f0de61b8c06f563003754a5be9
Shagaev Sergey <shagaevsergey48@gmail.com>
2025-01-03 02:20:29 +0300

    пока не работает

---
+++ InfixToPostfix.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include "StackList.h"
+
+using namespace std;
+
+
+bool preor(char s,StackList<char>& stack)//true-вытаскиваем все операторы с большим или равным приоритетом
+{
+	if (stack.isEmpty())
+		return false;
+	else if (s == '*' || s == '/')
+	{
+		if (stack.peek() == '*' || stack.peek() == '/')
+			return true;
+		else return false;
+	}
+	else if (s == '+' || s == '-')
+	{
+		if (stack.peek() == '(')
+			return false;
+		else true;
+	}
+	return false;
+}
+
+string InfixToPostfix()
+{
+	setlocale(LC_ALL, "ru");
+	cout << "Введите ф-лу:";
+	string str,newStr;
+	getline(cin, str);
+	StackList<char> stack;
+	for (int i = 0;i < str.length();i++)
+	{
+		if (str[i] != '+' && str[i] != '-' && str[i] != '/' && str[i] != '*' && str[i] != ')' && str[i] != '(')
+			newStr += str[i];
+		else
+		{
+			if (str[i] == '(')
+				stack.addElem(str[i]);
+			else if (str[i] == ')')
+			{
+				while (!stack.isEmpty() && stack.peek() != '(')
+				{
+					newStr += stack.peek();
+					stack.removeElem();
+				}
+				stack.removeElem();
+			}
+			else
+			{
+				while (!stack.isEmpty() && preor(str[i],stack))
+				{
+					newStr += stack.peek();
+					stack.removeElem();
+				}
+				stack.addElem(str[i]);
+			}
+		}
+	}
+	while (!stack.isEmpty())
+	{
+		newStr += stack.peek();
+		stack.removeElem();
+	}
+
+	return newStr;
+}
--- Source.cpp
+++ Source.cpp
@@ -5,29 +5,8 @@
 #include "StackArr.h"
 
 bool BracketBalance();
-
+bool InfixToPostfix();
 int main() {
 	
-	StackArr<int> stack(5);
-	stack.addElem(1);
-	stack.addElem(2);
-	stack.addElem(3);
-	stack.addElem(4);
-	stack.addElem(5);
-	stack.addElem(6);
-	stack.addElem(7);
-	stack.addElem(8);
-	stack.addElem(9);
-	stack.addElem(10);
-	stack.removeElem();
-	stack.removeElem();
-	stack.removeElem();
-	stack.removeElem();
-	stack.removeElem();
-	cout << stack.peek();
-	//stack.clear();
-	//cout << stack.isEmpty();
-	//cout << stack.peek();
-	//stack.print();
-	//stack.print();
+	cout << InfixToPostfix();
 }
