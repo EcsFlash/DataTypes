4bb15b13a6623f663c09b2a948aca17e7d44aacf
Shagaev Sergey <shagaevsergey48@gmail.com>
2025-01-04 02:23:31 +0300

    удалил проверку на пустоту в ф-иях добавления и удаления после звена

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,9 @@
 #pragma once
 #include "InfixToPostfix.cpp"
 #include "Calculate_formula.cpp"
+#include "LListWH.h"
 int main() {
 	
-	cout << Calculator_formula();
+	//cout << Calculator_formula();
+	LListWH<int> list;
 }
