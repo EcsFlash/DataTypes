8222c2798578f718919cc6967b29257127bda57b
EcsFlash <55760802+EcsFlash@users.noreply.github.com>
2025-01-04 13:46:12 +0300

    добавил Circular Double Linked List

--- Source.cpp
+++ Source.cpp
@@ -2,9 +2,13 @@
 #include "InfixToPostfix.cpp"
 #include "Calculate_formula.cpp"
 #include "LListWH.h"
+#include "DLListCircular.h"
 int main() {
 	
 	//cout << Calculator_formula();
-	LListWH<int> list(new int [5] {1,2,3,4,5},5,false);
-	list.print();
+	/*LListWH<int> list(new int [5] {1,2,3,4,5},5,false);
+	list.print();*/
+
+	CDLList<int> cdlist(new int[5] {1, 5, 3, 2, 4}, 5);
+	cdlist.print(true);
 }
